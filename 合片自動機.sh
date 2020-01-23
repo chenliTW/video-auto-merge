1b709cd16a9cbfa4682b9c296f5ce8a786eb8a05
ChenliTW <ab0897867564534231@gmail.com>
2020-01-24 00:57:07 +0800

    init2

---
+++ 合片自動機.sh
@@ -0,0 +1,16 @@
+chk=0
+cat $1 | while read line
+do
+   if [ "$line" == "---" ]; then
+      echo "ffmpeg -f concat -safe 0 -i "$filename.txt" -c copy "$filename.mp4" &" 
+      ffmpeg -f concat -safe 0 -i "$filename.txt" -c copy "$filename.mp4" & > /dev/null
+      chk="0"
+   elif [ "$chk" == "0" ];then
+      filename=`echo $line | tr -d '"'`
+      chk="1"
+      rm $filename.txt
+   else
+      echo file `echo $line | tr -d '"'` >> $filename.txt
+   fi
+done
+
