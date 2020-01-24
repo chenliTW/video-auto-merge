838d292299dc1cb15027846c33a5823523623ba1
ChenliTW <ab0897867564534231@gmail.com>
2020-01-24 11:43:06 +0800

    update

--- 分割器.sh
+++ 分割器.sh
@@ -2,12 +2,12 @@ chk=0
 cat $1 | while read line
 do
    if [ "$line" == "---" ]; then
-      echo "ffmpeg -i $infile -vcodec copy -acodec copy -ss $starttime -t $endtime $outfile &"
+      ffmpeg -i $infile -vcodec copy -acodec copy -ss $starttime -t $endtime $outfile &
       chk=0
    elif [ $chk -eq 4 ];then
-      echo "ffmpeg -i $infile -vcodec copy -acodec copy -ss $starttime -t $endtime $outfile &"
+      ffmpeg -i $infile -vcodec copy -acodec copy -ss $starttime -t $endtime $outfile &
       outfile=`echo $line | tr -d '"'`
-	  chk=2
+      chk=2
    elif [ $chk -eq 0 ];then
       infile=`echo $line | tr -d '"'`
       chk=1
