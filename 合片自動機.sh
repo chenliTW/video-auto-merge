b7a95bbd61a2adfcd22a041b127136ca04db3ccb
ChenliTW <ab0897867564534231@gmail.com>
2020-01-24 14:27:39 +0800

    bug fix

--- 合片自動機.sh
+++ 合片自動機.sh
@@ -3,7 +3,7 @@ cat $1 | while read line
 do
    if [ "$line" = "---" ]; then
       echo "ffmpeg -f concat -safe 0 -i "$filename.txt" -c copy "$filename" &" 
-      ffmpeg -f concat -safe 0 -i "$filename.txt" -c copy "$filename.mp4" & > /dev/null
+      ffmpeg -f concat -safe 0 -i "$filename.txt" -c copy "$filename" & > /dev/null
       chk="0"
    elif [ "$chk" = "0" ];then
       filename=`echo $line | tr -d '"'`
