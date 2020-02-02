2fa141d2b40dd67f02bc33e18f30fd64292b8f50
Samuel C.A. Lee <iamsamuellee92@gmail.com>
2020-02-02 13:27:30 +0800

    fix bug

--- split.sh
+++ split.sh
@@ -2,10 +2,10 @@ chk=0
 cat $1 | while read line
 do
    if [ "$line" = "---" ]; then
-      ffmpeg -i $infile -vcodec copy -acodec copy -ss $starttime -t $endtime $outfile &
+      ffmpeg -y -i $infile -vcodec copy -acodec copy -ss $starttime -to $endtime $outfile &
       chk=0
    elif [ $chk -eq 4 ];then
-      ffmpeg -i $infile -vcodec copy -acodec copy -ss $starttime -t $endtime $outfile &
+      ffmpeg -y -i $infile -vcodec copy -acodec copy -ss $starttime -to $endtime $outfile &
       outfile=`echo $line | tr -d '"'`
       chk=2
    elif [ $chk -eq 0 ];then
@@ -13,7 +13,7 @@ do
       chk=1
    elif [ $chk -eq 1 ];then
       outfile=`echo $line | tr -d '"'`
-	  chk=2
+	   chk=2
    elif [ $chk -eq 2 ];then
       starttime=`echo $line | tr -d '"'`
       chk=3
