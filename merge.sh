194831109b734f673c196edd591ac8a51ae730cc
Samuel C.A. Lee <iamsamuellee92@gmail.com>
2020-02-02 15:01:56 +0800

    bug fix

--- merge.sh
+++ merge.sh
@@ -1,9 +1,11 @@
 chk=0
+tempfile=""
 while read line
 do
    if [ "$line" = "---" ]; then
       echo "ffmpeg -f concat -safe 0 -i "$filename.txt" -c copy "$filename" &" 
       ffmpeg -y -f concat -safe 0 -i "$filename.txt" -c copy "$filename" & > /dev/null
+      tempfile="$tempfile $filename.txt"
       chk="0"
    elif [ "$chk" = "0" ];then
       filename=`echo $line | tr -d '"'`
@@ -14,4 +16,4 @@ do
    fi
 done < $1
 wait
-
+rm $tempfile
